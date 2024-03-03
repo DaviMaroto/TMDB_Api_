6f2578c4bd5f751306cc6db313e3fe7e5a8f1f5b
DaviMaroto <davimaroto09@gmail.com>
2024-03-03 11:14:28 -0300

    Adicionando icones e criando formulário

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -1,18 +1,13 @@
 import './App.css'
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 function App() {
 
   return (
   <div className="App">
-    <nav id='navbar'>
-      <h2>
-      <Link to="/">MoviesLibs</Link>
-      </h2>
-      <Link to="/movie/1">Movie</Link>
-      <Link to="/search">Search</Link>
-    <Outlet/>
-    </nav>
+    <Navbar/>
     <h2>Movies</h2>
+    <Outlet/>
   </div>
   )
 }
