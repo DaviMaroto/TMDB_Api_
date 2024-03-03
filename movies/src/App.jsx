fc16fef4124550e6dad467cd72750da1fbc8d575
DaviMaroto <davimaroto09@gmail.com>
2024-03-03 10:59:27 -0300

    Configurando navbar com os links

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
-
+import { Link, Outlet } from 'react-router-dom'
 function App() {
 
   return (
   <div className="App">
+    <nav id='navbar'>
+      <h2>
+      <Link to="/">MoviesLibs</Link>
+      </h2>
+      <Link to="/movie/1">Movie</Link>
+      <Link to="/search">Search</Link>
+    <Outlet/>
+    </nav>
     <h2>Movies</h2>
   </div>
   )
