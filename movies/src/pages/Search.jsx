1c3665fbdedcbf58bea89e6a844ec9042c659ca4
DaviMaroto <davimaroto09@gmail.com>
2024-05-31 23:47:57 -0300

    Adicionando funcionalidade no search da API

--- movies/src/pages/Search.jsx
+++ movies/src/pages/Search.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import MovieCard from "../components/MovieCard"
+
+import "./MoviesGrid.css"
+
+const seachUrl = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY 
+
+
 const Search = () => {
+    const [searchParams] = useSearchParams()
+
+    const [movies, setMovies] = useState([])
+    const query = searchParams.get("q")
+   
     return (
-      <div>Seach</div>
+      <div className="container">
+      <h2 className="title">Resultados: <span className="query-text">{query}</span></h2>
+      <div className="movies-container">
+      {movies.length === 0 && <p>Carregando...</p>}
+      {movies.length > 0 && 
+        movies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+      </div>
+    </div>
     )
   }
   
